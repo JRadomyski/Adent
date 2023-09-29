955b8c6bd39a96d83a9360a763391f750ee5ac49
Jakub Radomyski <zakubrad@gmail.com>
2023-09-30 00:15:34 +0200

    JavaScript: refactoring

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-//footer
 const yearInFooter = document.querySelector('.footer__year');
 const menuBtn = document.getElementById("menubtn");
 const sideNav = document.getElementById("sideNav");
@@ -8,14 +7,13 @@ const menu = document.getElementById("menu");
 sideNav.style.right = "-250px";
 
 
-menuBtn.onclick = function(){
-    if(sideNav.style.right == "-250px"){
+menuBtn.onclick = function () {
+    if (sideNav.style.right == "-250px") {
         sideNav.style.right = "0";
-        menu.src="img/icons/x.svg";
-    }
-    else{
-        sideNav.style.right="-250px";
-        menu.src="img/icons/menu.svg"
+        menu.src = "img/icons/x.svg";
+    } else {
+        sideNav.style.right = "-250px";
+        menu.src = "img/icons/menu.svg"
     }
 }
 
